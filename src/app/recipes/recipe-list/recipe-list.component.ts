14ff50cd0b2fa259b6f53e442b64138c3fc944bb
Shuhao Chen <shuhao.chen@sjsu.edu>
2018-05-23 14:34:06 -0700

    recipeandshoppinglist

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+    recipes: Recipe[] = [
+        new Recipe('testItem','item description', 'http://cyber-breeze.com/wp-content/uploads/2017/02/featured-image-39.jpg'),
+        new Recipe('testItem','item description', 'http://cyber-breeze.com/wp-content/uploads/2017/02/featured-image-39.jpg')
+    ];
   constructor() { }
 
   ngOnInit() {
