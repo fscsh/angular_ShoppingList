5e9f04334caa85601392473b2c0d07825ef69ce2
Shuhao Chen <shuhao.chen@sjsu.edu>
2018-05-23 17:44:47 -0700

    add

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
     recipes: Recipe[] = [
         new Recipe('testItem','item description', 'http://cyber-breeze.com/wp-content/uploads/2017/02/featured-image-39.jpg'),
-        new Recipe('testItem','item description', 'http://cyber-breeze.com/wp-content/uploads/2017/02/featured-image-39.jpg')
+        new Recipe('testItem222','item description222', 'http://cyber-breeze.com/wp-content/uploads/2017/02/featured-image-39.jpg')
     ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe){
+      this.recipeWasSelected.emit(recipe);
+  }
 }
