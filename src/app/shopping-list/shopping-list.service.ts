c687ab429f920719d222b20ea508adb232149016
Shuhao Chen <shuhao.chen@sjsu.edu>
2018-05-30 11:43:50 -0700

    shoppingListService

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,20 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
+
+export class ShoppingListService{
+    ingredientChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients : Ingredient[] = [
+        new Ingredient('Apple', 5),
+        new Ingredient('Banana',10),
+        new Ingredient('April',15)
+    ];
+    getIngredients(){
+        return this.ingredients.slice();
+    }
+    addIngredient(ingredient: Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientChanged.emit(this.ingredients.slice());
+    }
+}
